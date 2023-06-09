bd01a6dbd22d1965736fa0cc373956a3ffc1376d
Evan Todd <evan@etodd.io>
2023-06-09 12:21:14 -0700

    use constant time comparison for basic auth password

--- middlewares.go
+++ middlewares.go
@@ -28,11 +28,12 @@ func middlewareStack(mws ...middleware) middlewares {
 }
 
 func basicAuthMiddleware(username, password string) middleware {
+	passwordBytes := []byte(password)
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			user, pass, ok := r.BasicAuth()
 
-			if !ok || user != username || pass != password {
+			if !ok || user != username || subtle.ConstantTimeCompare([]byte(pass), passwordBytes) != 1 {
 				w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
 				w.WriteHeader(http.StatusUnauthorized)
 				w.Write([]byte("Unauthorized"))
